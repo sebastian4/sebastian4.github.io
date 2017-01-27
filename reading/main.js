18b7c1c8e1c1b4717cdb12ed483713b2fac61d07
Sebastian Lopez <slopez@42six.com>
2017-01-27 13:01:30 -0500

    fix to include more interacts

--- reading/main.js
+++ reading/main.js
@@ -1,7 +1,17 @@
 $(function () {
 ////
 
-interact('.draggable')  
+var voices = ["US English Female", "UK English Female", "UK English Male", "Australian Female"];
+voiceIndex = 0;
+
+var colors = ["#FF0000","#F1C40F","#00FF00","#008000","#008000","#FF00FF","#800080","#FFA07A","#00FFFF","#F1948A"];
+
+var flickerimages = true;
+var flickerIndex = 0;
+
+////
+
+  interact('.draggable')  
   .draggable({
     // enable inertial throwing
     inertia: true,
@@ -22,15 +32,30 @@ interact('.draggable')
     // enable autoScroll
     autoScroll: true,
     // call this function on every dragmove event
-    onmove: dragMoveListener,
-    onstart: dragStartListener
+    onmove: draggableDragMoveListener,
+    onstart: draggableDragStartListener
   });
   
-  interact('.draggable')
-    .on('doubletap', doubleTapListener);
+  ////
   
-  function dragStartListener (event) {
-    // console.log("drag start");
+  interact('.draggable')
+    .on('doubletap', draggableDoubleTapListener)
+    .on('tap', draggableOneTapListener);
+    
+  interact('.voice')
+    .on('doubletap', voiceDoubleTapListener)
+    .on('tap', voiceOneTapListener)
+    .on('hold', resetClones);
+    
+  interact('.imagezone')
+    .on('doubletap', imageDoubleTapListener)
+    .on('tap', imageOneTapListener)
+    .on('hold', resetClones);
+      
+  ////
+  
+  function draggableDragStartListener (event) {
+    //console.log("drag start");
     
     if (!event.target.dragOrigin) {
         // console.log("cloning");
@@ -53,21 +78,7 @@ interact('.draggable')
     }
   }
   
-  function doubleTapListener (event) {
-      console.log("double tap");
-
-      dragTargetParent = event.target.parentNode;
-    //   dragTargetParent.style.backgroundColor="gray";
-    
-      console.log(dragTargetParent.className);
-      
-      if (dragTargetParent.className.includes("aclone")) {
-        dragTargetParent.parentNode.removeChild(dragTargetParent);
-      }
-
-  }
-
-  function dragMoveListener (event) {
+  function draggableDragMoveListener (event) {
     var target = event.target,
         // keep the dragged position in the data-x/data-y attributes
         x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
@@ -82,116 +93,228 @@ interact('.draggable')
     target.setAttribute('data-x', x);
     target.setAttribute('data-y', y);
   }
+  
+  ////
+  
+  function draggableDoubleTapListener (event) {
+      //console.log("draggable double tap");
 
-// enable draggables to be dropped into this
-interact('.dropzone').dropzone({  
-  // Require a 50% element overlap for a drop to be possible
-  overlap: 0.50,
+      dragTargetParent = event.target.parentNode;
+    
+      //console.log(dragTargetParent.className);
+      
+      if (dragTargetParent.className.includes("aclone")) {
+        dragTargetParent.parentNode.removeChild(dragTargetParent);
+      }
 
-  // listen for drop related events:
-
-  ondropactivate: function (event) {
-    // add active dropzone feedback
-    event.target.classList.add('drop-active');
-  },
-  ondragenter: function (event) {
-    var draggableElement = event.relatedTarget,
-        dropzoneElement = event.target;
-
-    // feedback the possibility of a drop
-    dropzoneElement.classList.add('drop-target');
-  },
-  ondragleave: function (event) {
-    // remove the drop feedback style
-    event.target.classList.remove('drop-target');
-  },
-  ondrop: function (event) {
-    // event.relatedTarget.textContent = 'Dropped';
-  },
-  ondropdeactivate: function (event) {
-    // remove active dropzone feedback
-    event.target.classList.remove('drop-active');
-    event.target.classList.remove('drop-target');
   }
-});
+  
+  function draggableOneTapListener (event) {
+      //console.log("draggable one tap");
 
-$( "#box-voice" ).click(function() {
-  // console.log( "box-voice .click() called" );
-  
-  var cloneString = findCloneString();
-  
-  // console.log(cloneString);
-  
-  responsiveVoice.speak(cloneString);
-});
-
-$( "#imagezone" ).click(function() {
-  // console.log( "imagezone .click() called" );
-  
-  var cloneString = findCloneString();
-  
-  // console.log(cloneString);
-
-  getImages(cloneString);
-});
-
-function findCloneString() {
-  
-  var allClones = [];
-  
-  $( ".aclone" ).each(function( index ) {
+      dragTargetParent = event.target.parentNode;
     
-    var letter = $( this ).text().replace(/[\n ]/g,'');
-    // console.log( index + "=" + letter );
-    
-    var position = $( this ).offset();
-    // console.log(position);
-    
-    allClones[Math.trunc(position.left)] = letter;
+      // console.log(dragTargetParent.className);
+      
+      if (dragTargetParent.className.includes("aclone")) {
+        var arandom = Math.floor((Math.random() * 10));
+        dragTargetParent.style.backgroundColor = colors[arandom];
+      }
+
+  }
+
+  ////
+  
+  // enable draggables to be dropped into this
+  interact('.dropzone').dropzone({  
+    // Require a 50% element overlap for a drop to be possible
+    overlap: 0.50,
+
+    // listen for drop related events:
+
+    ondropactivate: function (event) {
+      // add active dropzone feedback
+      event.target.classList.add('drop-active');
+    },
+    ondragenter: function (event) {
+      var draggableElement = event.relatedTarget,
+          dropzoneElement = event.target;
+
+      // feedback the possibility of a drop
+      dropzoneElement.classList.add('drop-target');
+    },
+    ondragleave: function (event) {
+      // remove the drop feedback style
+      event.target.classList.remove('drop-target');
+    },
+    ondrop: function (event) {
+      // event.relatedTarget.textContent = 'Dropped';
+    },
+    ondropdeactivate: function (event) {
+      // remove active dropzone feedback
+      event.target.classList.remove('drop-active');
+      event.target.classList.remove('drop-target');
+    }
   });
   
-  var cloneString = "";
-  for (x in allClones) {
-    if (typeof(allClones[x]) != "undefined") {
-      cloneString = cloneString.concat(allClones[x]);
-    }
-  }
-  
-  return cloneString;
-}
+  ////
 
-function getImages(word) {
-  // console.log("getImages "+word);
-  
-  if (word == "") {
-    return;
-  }
-  
-  $.ajax({
-    type: "GET",
-    dataType: "jsonp",
-    url: "https://www.googleapis.com/customsearch/v1",
-    data: {
-      key: "AIzaSyCzb6SI_JRrp6xLLYV617Ary6n59h36ros",
-      cx: "004286675445984025592:ypgpkv9fjd4",
-      filter: "1",
-      searchType: "image",
-      //imgSize: "small",
-      q: word
-    }
-  }).done(function(data) {
-
-    // console.log("success");
-
-    var googleResults = data.items;
-    var link = googleResults[0].link;
-    // console.log(link);
+  function voiceOneTapListener (event) {
+    //console.log( "voice one tap" );
     
-    $("#zone-image").attr("src", link);
+    var cloneString = findCloneString();
+    
+    // console.log(cloneString);
+    
+    responsiveVoice.speak(cloneString, voices[voiceIndex]);
+  }
 
-  });
+  function voiceDoubleTapListener (event) {
+    //console.log( "voice double tap" );
+    voiceIndex++;
+    if (voiceIndex > 3) {
+      voiceIndex = 0;
+    }
+    responsiveVoice.speak(voices[voiceIndex], voices[voiceIndex]);
+    
+  }
   
-}
+  ////
+
+  function imageOneTapListener (event) {
+    //console.log( "image one tap" );
+    
+    var cloneString = findCloneString();
+    
+    // console.log(cloneString);
+
+    getImages(cloneString);
+  }
+
+  function imageDoubleTapListener (event) {
+    //console.log( "image double tap" );
+    
+    flickerimages = !flickerimages;
+    
+  }
+  
+  ////
+  
+  function resetClones (event) {
+    //console.log( "reset all clones x" );
+    
+    removeAllClones();
+    
+  }
+  
+  ////
+  
+  function removeAllClones() {
+    
+    $( ".aclone" ).each(function( index ) {
+      $(this).remove();
+    });
+    
+  }
+
+  function findCloneString() {
+    
+    var allClones = [];
+    
+    $( ".aclone" ).each(function( index ) {
+      
+      var letter = $( this ).text().replace(/[\n ]/g,'');
+      // console.log( index + "=" + letter );
+      
+      var position = $( this ).offset();
+      // console.log(position);
+      
+      allClones[Math.trunc(position.left)] = letter;
+    });
+    
+    var cloneString = "";
+    for (x in allClones) {
+      if (typeof(allClones[x]) != "undefined") {
+        cloneString = cloneString.concat(allClones[x]);
+      }
+    }
+    
+    return cloneString;
+  }
+
+  function getImages(word) {
+    // console.log("getImages "+word);
+    
+    if (word == "") {
+      return;
+    }
+    
+    if (flickerimages) {
+      
+      $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
+      {
+        tags: word,
+        tagmode: "any",
+        format: "json"
+      },
+      function(data) {
+        //console.log("success");
+
+        if (typeof(data)!="undefined" && typeof(data.items)!="undefined") {
+          
+          var link = data.items[flickerIndex].media.m;
+          $("#zone-image").attr("src", link);
+          
+          flickerIndex++;
+          if (flickerIndex > 6) {
+            flickerIndex = 0;
+          }
+        }
+        else {
+          //console.log("flicker image responses were undefined");
+          $("#zone-image").attr("src", "");
+        }
+        
+      });
+      
+    }
+    else {
+      
+      $.ajax({
+        type: "GET",
+        dataType: "jsonp",
+        url: "https://www.googleapis.com/customsearch/v1",
+        data: {
+          key: "AIzaSyCzb6SI_JRrp6xLLYV617Ary6n59h36ros",
+          cx: "004286675445984025592:ypgpkv9fjd4",
+          filter: "1",
+          searchType: "image",
+          //imgSize: "small",
+          q: word
+        }
+      }).done(function(data) {
+      
+        // console.log("success");
+        
+        if (typeof(data)!="undefined" && typeof(data.items)!="undefined") {
+          
+          var googleResults = data.items;
+          var link = googleResults[0].link;
+          //console.log(link);
+          
+          $("#zone-image").attr("src", link);
+          
+        }
+        else {
+          //console.log("google image responses were undefined");
+          $("#zone-image").attr("src", "");
+        }
+      
+      });
+      
+    }
+
+  }
     
 ////	
 });
