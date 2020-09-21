484bc7425b4c0383a42267a836311c445849fa2c
sebastian <sebastian.lopez.xx@gmail.com>
2020-09-21 16:59:07 -0400

    change to urls

--- quotes/precache-manifest.0889106244bb12efc4b98c315d83e10e.js
+++ quotes/precache-manifest.0889106244bb12efc4b98c315d83e10e.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "8c8f3ba48a4ac1e43c0dca47f92e1fda",
-    "url": "/index.html"
+    "url": "/quotes/index.html"
   },
   {
     "revision": "1f6ae9587e85e5ee2297",
-    "url": "/static/css/main.00a80998.chunk.css"
+    "url": "/quotes/static/css/main.00a80998.chunk.css"
   },
   {
     "revision": "985c72613406d8443b63",
-    "url": "/static/js/2.d0a62939.chunk.js"
+    "url": "/quotes/static/js/2.d0a62939.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/static/js/2.d0a62939.chunk.js.LICENSE.txt"
+    "url": "/quotes/static/js/2.d0a62939.chunk.js.LICENSE.txt"
   },
   {
     "revision": "1f6ae9587e85e5ee2297",
-    "url": "/static/js/main.a83d07b4.chunk.js"
+    "url": "/quotes/static/js/main.a83d07b4.chunk.js"
   },
   {
     "revision": "febdbcf3973fa193d5ea",
-    "url": "/static/js/runtime-main.f55c837b.js"
+    "url": "/quotes/static/js/runtime-main.f55c837b.js"
   }
 ]);
